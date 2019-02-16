17202a7f969acff26903e184e28c70c74c712f1b
Bart Bucknill <bartbucknill@hotmail.com>
2019-02-16 16:19:24 +0000

    Add bolt db handler

--- students/bart/handler.go
+++ students/bart/handler.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/boltdb/bolt"
 	"github.com/go-yaml/yaml"
 )
+
+// BoltDBHandler returns an http.HandlerFunc which gets the url corresponding to paths
+// from the DB, and redirects the http request.
+func BoltDBHandler(db *bolt.DB, fallback http.Handler) (http.HandlerFunc, error) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		var bytesURL []byte
+		db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("PathsToURLs"))
+			bytesURL = b.Get([]byte(path))
+			return nil
+		})
+		URL := string(bytesURL[:])
+		if len(URL) != 0 {
+			http.Redirect(w, r, URL, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}, nil
+}
 
 // MapHandler will return an http.HandlerFunc that will map any
 // paths (keys in the map) to their corresponding URL (values
@@ -14,7 +34,6 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r)
 		path := r.URL.Path
 		if dest, ok := pathsToUrls[path]; ok {
 			http.Redirect(w, r, dest, http.StatusFound)
--- students/bart/main.go
+++ students/bart/main.go
@@ -3,20 +3,44 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/boltdb/bolt"
 )
+
+func init() {
+	// create/open boltdb and seed with path and url
+	db, err := bolt.Open("url-shortener.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	defer db.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("PathsToURLs"))
+		if err != nil {
+			return fmt.Errorf("failed to create bucket: %v", err)
+		}
+		if err := b.Put([]byte("/urlshort-bolt"), []byte("https://github.com/gophercises/urlshort")); err != nil {
+			return fmt.Errorf("failed to insert data: %v", err)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	mux := defaultMux()
 
-	// Build the MapHandler using the mux as the fallback
+	// MapHandler with default mux as fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 	mapHandler := MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
+	// YAML handler with maphandler as fallback
 	yaml := `
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
@@ -24,11 +48,17 @@ func main() {
   url: https://github.com/gophercises/urlshort/tree/solution
 `
 	yamlHandler, err := YAMLHandler([]byte(yaml), mapHandler)
+
+	// Bolt db handler with YAML handler as fallback
+	db, err := bolt.Open("url-shortener.db", 0600, &bolt.Options{ReadOnly: true})
+	defer db.Close()
+	boltDBHandler, err := BoltDBHandler(db, yamlHandler)
+
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", boltDBHandler)
 }
 
 func defaultMux() *http.ServeMux {
